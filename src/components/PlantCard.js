5d6f58b5c8d0527b1ca1d407af3faf4231d524a9
Ziru Zou <jiyucoding@yahoo.com>
2023-02-08 01:11:45 -0500

    add Delete&Edit btn with onClick event, create handleDelete with DELETE rq, call onDeletePlant CB inside passing the response as arg

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function PlantCard( { plant } ) {
+function PlantCard( { plant, onDeletePlant } ) {
    
   const {id, name, image, price} = plant
 
@@ -13,12 +13,20 @@ function PlantCard( { plant } ) {
     setIsInSotck(!isInStock)
   }
 
+  function handleDelete() {
+    fetch(`http://localhost:6001/plants/${id}`, {method: "DELETE"})
+    .then(res => res.json())
+    .then(() => onDeletePlant(plant))
+  }
+
   return (
     <li className="card" id={id}>
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
       <button className={btnColor} onClick={handleStock}>{content}</button>
+      <button className="primary" onClick={handleDelete}>Delete</button>
+      <button className="primary">Edit</button>
     </li>
   );
 }
