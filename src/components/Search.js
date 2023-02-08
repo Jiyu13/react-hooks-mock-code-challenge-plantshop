3e8f9ebe77d2d9f2f96144109107bb643a392ea5
Ziru Zou <jiyucoding@yahoo.com>
2023-02-08 00:38:59 -0500

    add onChange event for search input, create helper function & call CB passing input value as arg

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,15 @@
 import React from "react";
 
-function Search() {
+function Search( { onSearchPlant } ) {
+
+  
+
+  function handleSearch(e) {
+    const search = e.target.value
+    onSearchPlant(search)
+
+  }
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
@@ -8,7 +17,7 @@ function Search() {
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={handleSearch}
       />
     </div>
   );
